e5770cf5bfc060d6c72fe018b944e2b1dd12008c
primexnev <nevzatonayjr@gmail.com>
2025-03-06 17:52:31 +0100

    Layercoding + adding

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/LayerTrigger.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/LayerTrigger.cs
@@ -2,26 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Cainos.PixelArtTopDown_Basic
+public class LayerTrigger : MonoBehaviour
 {
-    //when object exit the trigger, put it to the assigned layer and sorting layers
-    //used in the stair objects for player to travel between layers
-    public class LayerTrigger : MonoBehaviour
+    public string newLayer; // Hedef Layer (Layer1, Layer2, Layer3 gibi)
+    public string newSortingLayer; // Hedef Sorting Layer (Layer1, Layer2, Layer3 gibi)
+    public int newOrderInLayer; // Yeni Order in Layer değeri
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        public string layer;
-        public string sortingLayer;
+        Debug.Log(">>> Trigger TETİKLENDİ! Nesne: " + other.gameObject.name);
 
-        private void OnTriggerExit2D(Collider2D other)
+        if (other.CompareTag("Player")) // Sadece oyuncu etkilesin
         {
-            other.gameObject.layer = LayerMask.NameToLayer(layer);
+            Debug.Log(">>> Oyuncu merdivene girdi. Layer değiştiriliyor...");
 
-            other.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = sortingLayer;
-            SpriteRenderer[] srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
-            foreach ( SpriteRenderer sr in srs)
+            // Yeni Layer'ı ayarla
+            int layerIndex = LayerMask.NameToLayer(newLayer);
+            if (layerIndex == -1)
             {
-                sr.sortingLayerName = sortingLayer;
+                Debug.LogError("HATA: " + newLayer + " isimli Layer bulunamadı!");
+            }
+            else
+            {
+                other.gameObject.layer = layerIndex;
+                Debug.Log("Yeni Layer: " + newLayer);
+            }
+
+            // Sorting Layer ve Order in Layer güncelle
+            SpriteRenderer sr = other.gameObject.GetComponent<SpriteRenderer>();
+            if (sr != null)
+            {
+                sr.sortingLayerName = newSortingLayer;
+                sr.sortingOrder = newOrderInLayer;
+                Debug.Log("Yeni Sorting Layer: " + newSortingLayer + " | Order in Layer: " + newOrderInLayer);
+            }
+            else
+            {
+                Debug.LogError("HATA: Oyuncunun SpriteRenderer bileşeni bulunamadı!");
+            }
+
+            // Çocuk nesnelerin de Sorting Layer'ını değiştir
+            SpriteRenderer[] srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer sprite in srs)
+            {
+                sprite.sortingLayerName = newSortingLayer;
+                sprite.sortingOrder = newOrderInLayer;
             }
         }
+    }
 
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log(">>> Karakter merdivenin üstünde: " + other.gameObject.name);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log(">>> Karakter merdivenden çıktı: " + other.gameObject.name);
     }
 }
