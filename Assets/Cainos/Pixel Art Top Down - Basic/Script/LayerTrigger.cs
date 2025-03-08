a2287ff1a98868da49f848062b4ed187bf9adb06
primexnev <nevzatonayjr@gmail.com>
2025-03-08 23:05:26 +0100

    Layer update not-done

--- Assets/Cainos/Pixel Art Top Down - Basic/Script/LayerTrigger.cs
+++ Assets/Cainos/Pixel Art Top Down - Basic/Script/LayerTrigger.cs
@@ -4,60 +4,85 @@ using UnityEngine;
 
 public class LayerTrigger : MonoBehaviour
 {
-    public string newLayer; // Hedef Layer (Layer1, Layer2, Layer3 gibi)
-    public string newSortingLayer; // Hedef Sorting Layer (Layer1, Layer2, Layer3 gibi)
-    public int newOrderInLayer; // Yeni Order in Layer değeri
+    [Header("Layer Geçiş Ayarları")]
+    public string layer1 = "Layer 1 - Base";   // En alt katman
+    public string layer2 = "Layer 2 - Middle"; // Orta katman
+    public string layer3 = "Layer 3 - Top";    // En üst katman
+    public int orderLayer1 = 1; // Layer 1 için Order in Layer
+    public int orderLayer2 = 2; // Layer 2 için Order in Layer
+    public int orderLayer3 = 3; // Layer 3 için Order in Layer
+
+    private Dictionary<string, string> previousSortingLayers = new Dictionary<string, string>();
+    private Dictionary<string, int> previousOrderLayers = new Dictionary<string, int>();
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(">>> Trigger TETİKLENDİ! Nesne: " + other.gameObject.name);
-
         if (other.CompareTag("Player")) // Sadece oyuncu etkilesin
         {
             Debug.Log(">>> Oyuncu merdivene girdi. Layer değiştiriliyor...");
 
-            // Yeni Layer'ı ayarla
-            int layerIndex = LayerMask.NameToLayer(newLayer);
-            if (layerIndex == -1)
+            // Karakterin mevcut Layer'ını kontrol et
+            string currentLayer = other.gameObject.layer == LayerMask.NameToLayer(layer1) ? layer1 :
+                                  other.gameObject.layer == LayerMask.NameToLayer(layer2) ? layer2 :
+                                  layer3; // Varsayılan olarak en üst Layer kabul edilir.
+
+            string newLayer = (currentLayer == layer1) ? layer2 :
+                              (currentLayer == layer2) ? layer3 :
+                              layer2; // Eğer en üst Layer’daysa geri Layer 2’ye düş
+
+            int newLayerIndex = LayerMask.NameToLayer(newLayer);
+            if (newLayerIndex == -1)
             {
                 Debug.LogError("HATA: " + newLayer + " isimli Layer bulunamadı!");
+                return; // Hata olursa kodu durdur
             }
-            else
-            {
-                other.gameObject.layer = layerIndex;
-                Debug.Log("Yeni Layer: " + newLayer);
-            }
+            
+            other.gameObject.layer = newLayerIndex;
+            Debug.Log("Yeni Layer: " + newLayer);
 
             // Sorting Layer ve Order in Layer güncelle
-            SpriteRenderer sr = other.gameObject.GetComponent<SpriteRenderer>();
-            if (sr != null)
+            SpriteRenderer[] spriteRenderers = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer sr in spriteRenderers)
             {
-                sr.sortingLayerName = newSortingLayer;
-                sr.sortingOrder = newOrderInLayer;
-                Debug.Log("Yeni Sorting Layer: " + newSortingLayer + " | Order in Layer: " + newOrderInLayer);
-            }
-            else
-            {
-                Debug.LogError("HATA: Oyuncunun SpriteRenderer bileşeni bulunamadı!");
+                // Eski Sorting Layer ve Order'ı sakla
+                if (!previousSortingLayers.ContainsKey(sr.gameObject.name))
+                {
+                    previousSortingLayers[sr.gameObject.name] = sr.sortingLayerName;
+                    previousOrderLayers[sr.gameObject.name] = sr.sortingOrder;
+                }
+
+                // Yeni Sorting Layer ve Order'ı ayarla
+                sr.sortingOrder = (newLayer == layer1) ? orderLayer1 :
+                                  (newLayer == layer2) ? orderLayer2 :
+                                  orderLayer3;
             }
 
-            // Çocuk nesnelerin de Sorting Layer'ını değiştir
-            SpriteRenderer[] srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
-            foreach (SpriteRenderer sprite in srs)
-            {
-                sprite.sortingLayerName = newSortingLayer;
-                sprite.sortingOrder = newOrderInLayer;
-            }
+            Debug.Log("Layer ve Sorting Order güncellendi: " + newLayer);
         }
-    }
-
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        Debug.Log(">>> Karakter merdivenin üstünde: " + other.gameObject.name);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log(">>> Karakter merdivenden çıktı: " + other.gameObject.name);
+        if (other.CompareTag("Player")) // Sadece oyuncu etkilesin
+        {
+            Debug.Log(">>> Karakter merdivenden çıktı, eski layer geri yükleniyor...");
+
+            // Eski Layer'ı geri yükle
+            SpriteRenderer[] spriteRenderers = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
+            foreach (SpriteRenderer sr in spriteRenderers)
+            {
+                if (previousSortingLayers.ContainsKey(sr.gameObject.name))
+                {
+                    sr.sortingLayerName = previousSortingLayers[sr.gameObject.name];
+                }
+
+                if (previousOrderLayers.ContainsKey(sr.gameObject.name))
+                {
+                    sr.sortingOrder = previousOrderLayers[sr.gameObject.name];
+                }
+            }
+
+            Debug.Log("Eski Layer geri yüklendi.");
+        }
     }
 }
